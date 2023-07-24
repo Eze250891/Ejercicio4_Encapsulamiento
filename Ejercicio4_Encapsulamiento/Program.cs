6a827e88dae19921e782e49bd457a5f84f4bd860
Ezequiel Di Giacomo <ezequiel250891@hotmail.com>
2023-07-24 11:27:22 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio4_Encapsulamiento/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio4_Encapsulamiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
